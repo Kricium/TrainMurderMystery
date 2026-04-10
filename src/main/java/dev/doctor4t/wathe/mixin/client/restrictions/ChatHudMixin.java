56cb40a993ef7635ce7d1f7be820d34c0c8397bb
Huankings <hdw15099973617@163.com>
2026-04-10 09:07:37 -0400

    修改了chathudmixin以便兼容打字机制

--- src/main/java/dev/doctor4t/wathe/mixin/client/restrictions/ChatHudMixin.java
+++ src/main/java/dev/doctor4t/wathe/mixin/client/restrictions/ChatHudMixin.java
@@ -2,7 +2,9 @@ package dev.doctor4t.wathe.mixin.client.restrictions;
 
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import dev.doctor4t.wathe.cca.GameWorldComponent;
 import dev.doctor4t.wathe.client.WatheClient;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.ChatHud;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,14 +13,19 @@ import org.spongepowered.asm.mixin.Mixin;
  * 处理聊天界面显示
  * 当玩家处于游戏中的时候屏蔽聊天界面
  * 以使用talk bubbles模组
+ * 这么写是为了给大嗓门和饕餮适配
+ * 不然就会出现大嗓门发动技能的时候和饕餮吞人的时候在饕餮肚子里的玩家和饕餮都能看得到聊天框历史消息了
+ *
  */
-
 @Mixin(ChatHud.class)
 public class ChatHudMixin {
     @WrapMethod(method = "render")
     public void wathe$disableChatRender(DrawContext context, int currentTick, int mouseX, int mouseY, boolean focused, Operation<Void> original) {
-        if (!WatheClient.shouldDisableChat()) {
-            original.call(context, currentTick, mouseX, mouseY, focused);
+        if (MinecraftClient.getInstance().player != null) {
+            GameWorldComponent gameWorldComponent = GameWorldComponent.KEY.get(MinecraftClient.getInstance().player.getWorld());
+            if (!WatheClient.isPlayerAliveAndInSurvival() || !gameWorldComponent.isRunning()) {
+                original.call(context, currentTick, mouseX, mouseY, focused);
+            }
         }
     }
 }
